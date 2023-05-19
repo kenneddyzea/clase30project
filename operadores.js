ca44898b1a6b3d5c39aa5b3a69baf4f837de30dd
kenneddyZea <kenneddyzea@gmail.com>
2023-05-19 19:27:51 -0300

    mipriComit

---
+++ operadores.js
@@ -0,0 +1,28 @@
+console.clear()
+    let numeroA = 20;
+let numeroB = 8;
+let numeroC ="20";
+let numeroD = "8";
+//console.log(numeroA + numeroB)
+//console.log('Resultado de la RESTA: ',numeroA - numeroB)
+//console.log('Resultado de la MULTIPLICACION: ',numeroA * numeroB)
+//console.log('Resultado de la DIVISION: ',numeroA / numeroB)
+//console.log('Resultado del RESTO DE LA DIVISION', numeroA % numeroB);
+//INCREMENTO EN UN UNO EL VALOR DE LA VARIABLE(NUMERICA)numeroA = numeroA + 1 // console.log(++numeroA);
+//DECREMENTO EN UN UNO EL VALOR DE LA VARIABLE(NUMERICA) numeroA = numero A - 1 // console.log(--numeroA)
+
+//console.log(numeroA == numeroB); //igualdad simple
+//console.log(typeof numeroB, typeof numeroD);
+//console.log(numeroB == numeroD); //igualdad estricta
+
+//console.log(numeroA != numeroB); //desigualdad simple
+//console.log(numeroB !== numeroD); //desigualdad estricta
+
+//console.log(numeroA > numeroC); //mayor
+//console.log(numeroA < numeroC); //menor
+//console.log(numeroA >= numeroC); //mayor o igual
+//console.log(numeroA <= numeroC); //menor o igual
+
+//console.log("a" + "b"); //concatenación
+//console.log("a" + "1"); //concatenación
+//console.log(1 + "a"); //concatenación
